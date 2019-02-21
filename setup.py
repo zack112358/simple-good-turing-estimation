54928a3abe733f8d31bb253b928c9fe38a886747
Zachary McCord <zjmccord@gmail.com>
2019-02-21 19:05:40 -0400

    pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name="simple-good-turing",
-    version="0.0.2",
+    version="0.0.3",
     author="Zachary McCord",
     author_email="zjmccord@gmail.com",
     description="Simple Good-Turing smoothing",
